ef41d52282af38c1a6281250b466347f9504076b
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-01 21:32:36 +0545

    added connectivityStatusProvider for domain repository of exercise

--- lib/features/exercise/domain/repository/exercise_repository.dart
+++ lib/features/exercise/domain/repository/exercise_repository.dart
@@ -1,12 +1,21 @@
 import 'package:dartz/dartz.dart';
 import 'package:final_assignment/core/common/provider/internet_connectivity.dart';
 import 'package:final_assignment/core/failure/failure.dart';
+import 'package:final_assignment/features/exercise/data/repository/exercise_local_repository.dart';
 import 'package:final_assignment/features/exercise/data/repository/exercise_remote_repository.dart';
 import 'package:final_assignment/features/exercise/domain/entity/exercise_entity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final exerciseRepositoryProvider = Provider<IExerciseRepository>(
-  checkConnectivity = ref.read(connectivityStatusProvider);
+  (ref) {
+    final checkConnectivity = ref.read(connectivityStatusProvider);
+
+    if (checkConnectivity == ConnectivityStatus.isConnected) {
+      return ref.read(exerciseRemoteRepositoryProvider);
+    } else {
+      return ref.read(exerciseLocalRepositoryProvider);
+    }    
+  },
 
 );
 
