d57670c20d5287b0582732dfa9dc1c7cdafdd386
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-01 21:29:35 +0545

    added connectivityStatusProvider for domain repository in auth

--- lib/features/auth/domain/repository/i_auth_repository.dart
+++ lib/features/auth/domain/repository/i_auth_repository.dart
@@ -1,18 +1,25 @@
 import 'dart:io';
 
 import 'package:dartz/dartz.dart';
+import 'package:final_assignment/core/common/provider/internet_connectivity.dart';
 import 'package:final_assignment/core/failure/failure.dart';
+import 'package:final_assignment/features/auth/data/repository/auth_local_repository.dart';
 import 'package:final_assignment/features/auth/data/repository/auth_remote_repository.dart';
 import 'package:final_assignment/features/auth/domain/entity/auth_entity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final authRepositoryProvider = Provider<IAuthRepository>((ref) {
+  final checkConnectivity = ref.read(connectivityStatusProvider);
+
+  if (checkConnectivity == ConnectivityStatus.isConnected) {
     return ref.read(authRemoteRepositoryProvider);
+  } else {
+    return ref.read(authLocalRepositoryProvider);
+  }
 });
 
 abstract class IAuthRepository {
   Future<Either<Failure, bool>> registerStudent(AuthEntity student);
   Future<Either<Failure, bool>> loginStudent(String email, String password);
   Future<Either<Failure, AuthEntity>> getCurrentUser();
-
 }
--- lib/features/exercise/domain/repository/exercise_repository.dart
+++ lib/features/exercise/domain/repository/exercise_repository.dart
@@ -1,11 +1,13 @@
 import 'package:dartz/dartz.dart';
+import 'package:final_assignment/core/common/provider/internet_connectivity.dart';
 import 'package:final_assignment/core/failure/failure.dart';
 import 'package:final_assignment/features/exercise/data/repository/exercise_remote_repository.dart';
 import 'package:final_assignment/features/exercise/domain/entity/exercise_entity.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final exerciseRepositoryProvider = Provider<IExerciseRepository>(
-  (ref) => ref.read(exerciseRemoteRepositoryProvider),
+  checkConnectivity = ref.read(connectivityStatusProvider);
+
 );
 
 abstract class IExerciseRepository {
