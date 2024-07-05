f12c7837b307aa43c084af942c968b412b6cc35c
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-06 00:06:59 +0545

    created proximity sensor feature

---
+++ lib/features/sensors/proximity.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+
+import 'package:all_sensors2/all_sensors2.dart';
+import 'package:flutter/material.dart';
+
+class ProximityScreen extends StatefulWidget {
+  const ProximityScreen({super.key});
+
+  @override
+  State<ProximityScreen> createState() => _ProximityScreenState();
+}
+
+class _ProximityScreenState extends State<ProximityScreen> {
+  double _proximityValue = 0;
+  final List<StreamSubscription<dynamic>> _streamSubscriptions =
+      <StreamSubscription<dynamic>>[];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _streamSubscriptions.add(proximityEvents!.listen((ProximityEvent event) {
+      setState(() {
+        _proximityValue = event.proximity;
+      });
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Proximity'),
+      ),
+      body: Center(
+        child: _proximityValue >= 4
+            ? const Text(
+                'Object is Near',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              )
+            : const Text(
+                'Object is Far',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+      ),
+    );
+  }
+}
