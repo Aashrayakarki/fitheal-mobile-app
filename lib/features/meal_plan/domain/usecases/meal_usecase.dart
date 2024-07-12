82a830c92b54aaf77c39d8daedb1bd0f49751108
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-12 18:22:32 +0545

    created state and view model for meal plans

--- lib/features/meal_plan/domain/usecases/meal_usecase.dart
+++ lib/features/meal_plan/domain/usecases/meal_usecase.dart
@@ -1,13 +1,11 @@
 import 'package:dartz/dartz.dart';
 import 'package:final_assignment/core/failure/failure.dart';
-import 'package:final_assignment/features/exercise/domain/entity/exercise_entity.dart';
-import 'package:final_assignment/features/exercise/domain/repository/exercise_repository.dart';
 import 'package:final_assignment/features/meal_plan/domain/entity/meal_entity.dart';
 import 'package:final_assignment/features/meal_plan/domain/repository/meal_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final mealUseCaseProvider = Provider<MealUseCase>(
-  (ref) => MealUseCase(
+final mealUseCaseProvider = Provider<MealUsecase>(
+  (ref) => MealUsecase(
     mealRepository: ref.read(mealRepositoryProvider),
   ),
 );
---
+++ lib/features/meal_plan/presentation/viewmodel/meal_view_model.dart
@@ -0,0 +1,96 @@
+import 'package:final_assignment/core/common/my_snackbar.dart';
+import 'package:final_assignment/features/exercise/domain/entity/exercise_entity.dart';
+import 'package:final_assignment/features/exercise/domain/usecases/exercise_usecase.dart';
+import 'package:final_assignment/features/exercise/presentation/state/exercise_state.dart';
+import 'package:final_assignment/features/meal_plan/domain/entity/meal_entity.dart';
+import 'package:final_assignment/features/meal_plan/domain/usecases/meal_usecase.dart';
+import 'package:final_assignment/features/meal_plan/presentation/state/meal_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final mealViewModelProvider =
+    StateNotifierProvider<MealViewModel, MealState>(
+  (ref) => MealViewModel(
+    ref.read(mealUseCaseProvider),
+  ),
+);
+
+class MealViewModel extends StateNotifier<MealState> {
+  final MealUsecase mealUseCase;
+
+Future resetState() async {
+    state = MealState.initial();
+    getAllMeals();
+  }
+
+  MealViewModel(this.mealUseCase) : super(MealState.initial()) {
+    getAllMeals();
+  }
+
+  deleteMeal(MealEntity meal) async {
+    state = state.copyWith(isLoading: true);
+    var data = await mealUseCase.deleteMeal(meal.mealId!);
+
+    data.fold(
+      (l) {
+        state = state.copyWith(isLoading: false, error: l.error);
+        showMySnackBar(message: l.error, color: Colors.red);
+      },
+      (r) {
+        state.lstMeals.remove(meal);
+        state = state.copyWith(isLoading: false, error: null);
+        showMySnackBar(
+          message: 'Meal deleted successfully',
+        );
+      },
+    );
+
+    getAllMeals();
+  }
+
+  addMeal(MealEntity meal) async {
+    state = state.copyWith(isLoading: true);
+    var data = await mealUseCase.addMeal(meal);
+
+    data.fold(
+      (l) {
+        state = state.copyWith(isLoading: false, error: l.error);
+        showMySnackBar(message: l.error, color: Colors.red);
+      },
+      (r) {
+        state = state.copyWith(isLoading: false, error: null);
+        showMySnackBar(message: 'Meal added successfully');
+      },
+    );
+
+    getAllMeals();
+  }
+
+  getAllMeals() async {
+    state = state.copyWith(isLoading: true);
+    final currentState = state;
+    final page = currentState.page + 1;
+    final lstMeals = currentState.lstMeals;
+    final hasReachedMax = currentState.hasReachedMax;
+
+    if (!hasReachedMax) {
+      var data = await mealUseCase.getAllMeals(page);
+
+      data.fold((l) {
+        state = state.copyWith(
+            hasReachedMax: true, isLoading: false, error: l.error);
+        showMySnackBar(message: l.error, color: Colors.red);
+      }, (r) {
+        if (r.isEmpty) {
+          state = state.copyWith(hasReachedMax: true, isLoading: false);
+        } else {
+          state = state.copyWith(
+              isLoading: false, meals: [...lstMeals, ...r], page: page);
+        }
+      });
+    }
+    if (hasReachedMax) {
+      state = state.copyWith(isLoading: false);
+    }
+  }
+}
