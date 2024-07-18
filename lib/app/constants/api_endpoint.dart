0df1bc2ae1806f56b7d919ae97e0acaf35644d92
Aashrayakarki <aashrayakarki7@gmail.com>
2024-07-18 17:27:02 +0545

    changed ip address format in api endpoint

--- lib/app/constants/api_endpoint.dart
+++ lib/app/constants/api_endpoint.dart
@@ -3,18 +3,16 @@ class ApiEndpoints {
 
   static const Duration connectionTimeout = Duration(seconds: 1000);
   static const Duration receiveTimeout = Duration(seconds: 1000);
-  static const String baseUrl = "http://192.168.1.4:5000/api/";
-  // static const String baseUrl = "http://10.12.17.71:5000/api/";
 
-  // static const String baseUrl = "http://localhost:3000/api/v1/";
-  //static const String baseUrl = "http://192.168.4.4:3000/api/v1/";
+  static const String ipAddress = "192.168.1.4:5000";
+  static const String baseUrl = "http://$ipAddress/api/";
+  static const String imageUrl = "http://$ipAddress/products/";
 
   // ====================== Auth Routes ======================
   static const String login = "user/login";
   static const String register = "user/register";
   static const String currentUser = "user/getMe";
   static const String uploadImage = "user/uploadVideo";
-  static const String imageUrl = "http://192.168.1.4:5000/products/";
   static const String forgotPassword = "user/forgot_password";
 
   // ====================== Exercise Routes ======================
